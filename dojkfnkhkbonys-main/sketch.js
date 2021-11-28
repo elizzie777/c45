95805e896d37527fec83d533009d702b0f73cc31
elizzie777 <85830044+elizzie777@users.noreply.github.com>
2021-11-28 17:55:44 +0000

    Add files via upload

---
+++ dojkfnkhkbonys-main/sketch.js
@@ -0,0 +1,28 @@
+var PLAY = 1
+var END = 0
+var gameState = PLAY
+var player, coin, spikes, heart, obstacle, gameover, playerimg, coinimg, spikesimg, heartimg, obstacleimg, gameoverimg, background1
+
+
+function preload(){
+playerimg = loadImage("pixil-frame-0 (2).png");
+coinimg = loadImage("pixil-frame-0 (6).png");
+spikesimg = loadImage("pixil-frame-0 (5).png");
+heartimg = loadImage("pixil-frame-0 (7).png");
+obstacleimg = loadImage("pixil-frame-0 (4).png");
+gameoverimg = loadImage("pixil-frame-0.png");
+
+}
+
+function setup(){
+    createCanvas(600, 200)
+
+   
+    
+    
+
+}
+
+function draw(){
+    background(0)
+}
